b86f6f396e78a44ffe43eba4c064cbcd1f31c240
Parish.S <144549353+Parish242@users.noreply.github.com>
2025-07-29 19:57:07 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,17 @@
-__import__('setuptools').setup()
+from setuptools import setup, find_packages
+
+setup(
+    name='csci_env',
+    version='0.1.0',
+    packages=find_packages(include=['env_extension']),
+    include_package_data=True,
+    install_requires=[
+        'jupyterlab',
+        'tornado'
+    ],
+    entry_points={
+        'jupyter_server.server_extensions': [
+            'env_extension = env_extension:setup_handlers'
+        ]
+    }
+)
